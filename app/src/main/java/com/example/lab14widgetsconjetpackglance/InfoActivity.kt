f62b5d5e108c7f74d1e835861e14c9fc85ec47ae
diegohuacca-hub <diegohuacca@tecsup.edu.pe>
2026-06-22 10:41:55 -0500

    feat: add InfoActivity and second button in widget

---
+++ app/src/main/java/com/example/lab14widgetsconjetpackglance/InfoActivity.kt
@@ -0,0 +1,27 @@
+﻿package com.example.lab14widgetsconjetpackglance
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.example.lab14widgetsconjetpackglance.ui.theme.Lab14WidgetsConJetpackGlanceTheme
+
+class InfoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lab14WidgetsConJetpackGlanceTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Vista de Información")
+                }
+            }
+        }
+    }
+}
